cd73e0fd4d241313ddcc259c6c8e6bd78a773344
AR-Fatma <90813489+AR-Fatma@users.noreply.github.com>
2024-06-14 04:08:12 -0400

    FizzBuzz to pass test

--- lab04/src/FizzBuzz.java
+++ lab04/src/FizzBuzz.java
@@ -1,5 +1,6 @@
 public class FizzBuzz {
     public String getFizzBuzz(int number) {
+        if (number % 3 == 0) return "Fizz";
         return Integer.toString(number);
     }
 }
--- lab04/test/FizzBuzzTest.java
+++ lab04/test/FizzBuzzTest.java
@@ -1,8 +1,5 @@
 package test;
 
-
-
-
 public class FizzBuzzTest {
     @Test
     public void testFizzBuzz() {
@@ -10,7 +7,7 @@ public class FizzBuzzTest {
         assertEquals("1", fb.getFizzBuzz(1));
         assertEquals("2", fb.getFizzBuzz(2));
         assertEquals("Fizz", fb.getFizzBuzz(3)); // This will fail as the method isn't implemented yet.
-        
+
        
     }
 }
